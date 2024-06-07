53c2387f9ca8d6b001861447fdfcc9bd62df5e4e
Nicola Vitucci <nicola.vitucci@neo4j.com>
2024-06-07 16:06:36 +0100

    Example of dynamic docstring

--- graphdatascience/caller_base.py
+++ graphdatascience/caller_base.py
@@ -6,11 +6,35 @@ from .query_runner.query_runner import QueryRunner
 from .server_version.server_version import ServerVersion
 
 
+all_endpoints = {
+    "gds.kmeans.stream"
+}
+
+
 class CallerBase(ABC):
     def __init__(self, query_runner: QueryRunner, namespace: str, server_version: ServerVersion):
         self._query_runner = query_runner
         self._namespace = namespace
         self._server_version = server_version
+
+        self.__name__ = f"{namespace}"
+
+        signature = """\n
+            Args:
+                param1: The first parameter.
+                param2: The second parameter.
+
+            Returns:
+                The return value. True for success, False otherwise.
+        """
+
+        if namespace in all_endpoints:
+            self.__doc__ = signature
+
+        if namespace + ".stream" in all_endpoints:
+            self.__custom_documentations__ = {
+                "stream": signature,
+            }
 
     def _raise_suggestive_error_message(self, requested_endpoint: str) -> NoReturn:
         list_result = self._query_runner.call_procedure(
